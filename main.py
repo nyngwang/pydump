ac7375ba7b2a8bd1012d16eadc74d548c8fc7582
nyngwang <nyngwang@gmail.com>
2023-04-22 04:45:45 +0800

    first course

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from pydump.test import hi
+from pydump.courses import no_oop
 
 
 if __name__ == '__main__':
-    hi.say_hello()
-    hi.say_hello()
+    # hi.say_hello()
+
+    no_oop.without_oop()
---
+++ pydump/courses/no_oop.py
@@ -0,0 +1,14 @@
+
+
+def without_oop():
+
+    item1 = 'Phone'
+    item1_price = 1000
+    item1_quantity = 10
+    item1_total_price = item1_price*item1_quantity
+
+
+    print(item1)
+    print(item1_price)
+    print(item1_quantity)
+    print(item1_total_price)
