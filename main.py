da076137edb50e5aaed752271f0927d2bec29a8e
nyngwang <nyngwang@gmail.com>
2023-04-22 04:55:35 +0800

    create an empty class

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 from pydump.test import hi
-from pydump.courses import no_oop
+from pydump.courses import (
+    no_oop,
+    yes_oop
+)
 
 
 if __name__ == '__main__':
     # hi.say_hello()
 
     no_oop.without_oop()
+    yes_oop.create_my_class()
+
---
+++ pydump/courses/yes_oop.py
@@ -0,0 +1,15 @@
+
+# Yup, we need to creat it outside
+
+class Item:
+    pass
+
+
+def create_my_class():
+
+    item1 = Item()
+    
+    print(item1)
+
+
+
