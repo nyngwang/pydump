eede43b86972f7c2d988261b519715a480cc1c19
nyngwang <nyngwang@gmail.com>
2023-04-23 05:08:19 +0800

    should always init the class attri

--- pydump/courses/yes_oop.py
+++ pydump/courses/yes_oop.py
@@ -7,12 +7,11 @@ import textwrap # TIL: built-in
 
 # NOTE: class is better to be created outside function.
 class Item:
-    # This will be neither a class/instance attribute
-    # if you don't provide a default value
-    is_rigid: bool = False
+    # without initialization, this class attribute will not exist.
+    pay_rate: int = 0.8
 
     @typechecked
-    def __init__(self, item: str, price: int, quantity=0):
+    def __init__(self, item: str, price: int, quantity=1):
         assert len(item) > 0, f"item should not have an empty name"
         assert price > 0, f"price {price} should be positive"
         assert quantity >= 0, f"quantity {quantity} should not be negative"
@@ -23,6 +22,9 @@ class Item:
 
     def calculate_total_price(self):
         return self.price * self.quantity
+
+    def apply_discount(self):
+        self.price = self.price * type(self).pay_rate
 
     def __str__(self) -> str:
 
@@ -42,27 +44,31 @@ class Item:
 def _compare_before_after_attri_init(item):
     # use `__dict__` to differentiate class/instance attributes
     print()
-    msg_template_found_in = "is_rigid is found in class:{}, instance:{}"
+    msg_template_found_in = "pay_rate is found in class:{}, instance:{}"
 
     print(msg_template_found_in.format(
-        'is_rigid' in  Item.__dict__.keys(),
-        'is_rigid' in  item.__dict__.keys()
+        'pay_rate' in  Item.__dict__.keys(),
+        'pay_rate' in  item.__dict__.keys()
     ))
 
-    print('setting item.is_rigid is set')
-    item.is_rigid = True
+    print('setting item.pay_rate is set')
+    item.pay_rate = 0.8
 
     print(msg_template_found_in.format(
-        'is_rigid' in  Item.__dict__.keys(),
-        'is_rigid' in  item.__dict__.keys()
+        'pay_rate' in  Item.__dict__.keys(),
+        'pay_rate' in  item.__dict__.keys()
     ))
 
 
 def create_my_class():
 
     item1 = Item('Phone', 1000)
-    
     _compare_before_after_attri_init(item1)
+
+    print(item1)
+    item1.apply_discount()
+    print(item1)
+    
 
 
 
