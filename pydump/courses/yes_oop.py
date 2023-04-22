5d3530b0cfed3c75e834ea9325a52edcb4bbbef8
nyngwang <nyngwang@gmail.com>
2023-04-23 06:56:45 +0800

    read from csv using `csv`

--- pydump/courses/yes_oop.py
+++ pydump/courses/yes_oop.py
@@ -1,3 +1,4 @@
+import csv
 
 # NOTE: `TypeGuard` from `typing` is just for type hinting.
 import textwrap # TIL: built-in
@@ -17,13 +18,27 @@ class Item:
             print(item)
 
 
+    @classmethod
+    def from_csv(cls):
+        with open('pydump/data/item.csv', 'r') as f:
+            reader = csv.DictReader(f)
+            items = list(reader)
+
+        for item in items:
+            cls(
+                name=item.get('name'),
+                price=int(item.get('price')),
+                quantity=int(item.get('quantity')),
+            )
+
+
     @typechecked
-    def __init__(self, item: str, price: int, quantity=1):
-        assert len(item) > 0, f"item should not have an empty name"
+    def __init__(self, name: str, price: int, quantity=1):
+        assert len(name) > 0, f"item should not have an empty name"
         assert price > 0, f"price {price} should be positive"
         assert quantity >= 0, f"quantity {quantity} should not be negative"
 
-        self.item = item
+        self.name = name
         self.price = price
         self.quantity = quantity
 
@@ -34,8 +49,10 @@ class Item:
     def calculate_total_price(self):
         return self.price * self.quantity
 
+
     def apply_discount(self):
         self.price = self.price * type(self).pay_rate
+
 
     def __repr__(self) -> str:
         message = """\
@@ -43,7 +60,7 @@ class Item:
         """
 
         return textwrap.dedent(message).format(
-            self.item,
+            self.name,
             self.quantity,
             self.calculate_total_price()
         )
@@ -83,10 +100,7 @@ def create_my_class():
     _compare_before_after_attri_init(item1)
     _get_discount_from_class_attr(item1)
 
-    item2 = Item('Phone', 2000)
-    item3 = Item('Phone', 3000)
-    item4 = Item('Phone', 4000)
-
+    Item.from_csv()
     Item.view_the_bag()
 
     
